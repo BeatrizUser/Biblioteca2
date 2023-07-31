d2111f43549bb14d23cc3cbdb96581f5ac5ab8ae
BeatrizUser <beatriz.machadosilva@gmail.com>
2023-07-31 00:58:08 -0300

    set models

--- biblioteca/admin.py
+++ biblioteca/admin.py
@@ -1,3 +1,62 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+# Registrar o modelo Livro
+@admin.register(Livro)
+class LivroAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'autor', 'editora', 'ano_public')
+    list_filter = ('autor', 'editora')
+    search_fields = ('titulo', 'autor')
+    def save_model(self, request, obj, form, change):
+        # Verifica se é um novo livro sendo criado (não uma edição)
+        if not obj.pk:
+            obj.save()  # Salva o livro para obter o ID gerado automaticamente
+            # Agora podemos exibir o código gerado
+            self.message_user(request, f'O código do livro gerado é: {obj.exemplar.codigo}', level='SUCCESS')
+        else:
+            obj.save()
+
+# Registrar o modelo Exemplar
+@admin.register(Exemplar)
+class ExemplarAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'livro', 'estante', 'prateleira', 'localizacao')
+    list_filter = ('estante', 'prateleira', 'livro__titulo')
+    search_fields = ('codigo', 'livro__titulo')
+
+# Registrar o modelo Emprestimo
+@admin.register(Emprestimo)
+class EmprestimoAdmin(admin.ModelAdmin):
+    list_display = ('cliente', 'data_emprestimo', 'data_devolucao')
+    list_filter = ('cliente__nome', 'data_emprestimo', 'data_devolucao')
+    search_fields = ('cliente__nome',)
+
+# Registrar o modelo Venda
+@admin.register(Venda)
+class VendaAdmin(admin.ModelAdmin):
+    list_display = ('cliente', 'exemplar', 'data_venda', 'preco')
+    list_filter = ('cliente__nome', 'data_venda')
+    search_fields = ('cliente__nome', 'exemplar__livro__titulo')
+
+# Registrar o modelo Funcionario
+@admin.register(Funcionario)
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cargo', 'email')
+    list_filter = ('cargo',)
+    search_fields = ('nome', 'cargo')
+
+# Registrar o modelo Estoque
+@admin.register(Estoque)
+class EstoqueAdmin(admin.ModelAdmin):
+    list_display = ('livro', 'quantidade')
+    list_filter = ('livro__titulo',)
+    search_fields = ('livro__titulo',)
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'endereco', 'email', 'imagem_preview')
+    search_fields = ('nome', 'endereco', 'email')
+
+    def imagem_preview(self, obj):
+        return obj.foto.url if obj.foto else None
+
+    imagem_preview.short_description = 'Foto'
--- biblioteca/models.py
+++ biblioteca/models.py
@@ -1,3 +1,71 @@
+import random
 from django.db import models
 
-# Create your models here.
+class Livro(models.Model):
+    titulo = models.CharField(max_length=100)
+    autor = models.CharField(max_length=100)
+    editora = models.CharField(max_length=100)
+    ano_public = models.IntegerField()
+
+    def __str__(self):
+        return self.titulo
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=100)
+    endereco = models.CharField(max_length=200)
+    email = models.EmailField()
+    foto = models.ImageField(upload_to='clientes/', null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
+class Exemplar(models.Model):
+    codigo = models.CharField(max_length=5, primary_key=True, unique=True, editable=False)
+
+    def _gerar_codigo_aleatorio(self):
+        return str(random.randint(10000, 99999))
+
+    def save(self, *args, **kwargs):
+        if not self.codigo:
+            self.codigo = self._gerar_codigo_aleatorio()
+        super().save(*args, **kwargs)
+
+    livro = models.ForeignKey(Livro, on_delete=models.CASCADE)
+    estante = models.CharField(max_length=50)
+    prateleira = models.CharField(max_length=50)
+    localizacao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.livro.titulo} - Exemplar {self.codigo}"
+
+class Emprestimo(models.Model):
+    data_emprestimo = models.DateTimeField(auto_now_add=True)
+    data_devolucao = models.DateTimeField()
+    exemplares = models.ManyToManyField(Exemplar)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Empréstimo para {self.cliente.nome} em {self.data_emprestimo}"
+
+class Venda(models.Model):
+    data_venda = models.DateTimeField(auto_now_add=True)
+    exemplar = models.ForeignKey(Exemplar, on_delete=models.CASCADE)
+    preco = models.FloatField()
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Venda de {self.exemplar.livro.titulo} em {self.data_venda}"
+
+class Funcionario(models.Model):
+    nome = models.CharField(max_length=100)
+    cargo = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.nome
+
+class Estoque(models.Model):
+    livro = models.ForeignKey(Livro, on_delete=models.CASCADE)
+    quantidade = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.livro.titulo} - Quantidade: {self.quantidade}"
