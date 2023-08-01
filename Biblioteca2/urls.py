eefd2910243246a2073bb016b2f3b49789297de2
BeatrizUser <beatriz.machadosilva@gmail.com>
2023-08-01 09:02:22 -0300

    livro detalhado

--- Biblioteca2/urls.py
+++ Biblioteca2/urls.py
@@ -6,6 +6,7 @@ from biblioteca import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('livro/<str:bookId>/', views.livro_detalhado, name='livro_detalhado'),
     path('pesquisar/', views.buscar_livro, name='buscar_livro'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- biblioteca/views.py
+++ biblioteca/views.py
@@ -6,15 +6,16 @@ from .models import Livro
 
 # Create your views here.
 def buscar_livro(request):
-    search_query = request.GET.get('q', '')
-    if search_query:
-        url = f'https://www.googleapis.com/books/v1/volumes?q={search_query}'
+    bookId = request.GET.get('q', '')
+    if bookId:
+        url = f'https://www.googleapis.com/books/v1/volumes?q={bookId}'
         response = requests.get(url)
         data = response.json()
 
         books = []
         if 'items' in data:
             for item in data['items']:
+                bookId = item['id']
                 titulo = item['volumeInfo'].get('title', 'Desconhecido')
                 autor = ', '.join(item['volumeInfo'].get('authors', ['Desconhecido']))
                 editora = item['volumeInfo'].get('publisher', 'Editora desconhecida.')
@@ -22,6 +23,7 @@ def buscar_livro(request):
                 cover_url = item['volumeInfo']['imageLinks']['thumbnail'] if 'imageLinks' in item['volumeInfo'] and 'thumbnail' in item['volumeInfo']['imageLinks'] else None
                 
                 book = {
+                    'id': bookId,
                     'title': titulo,
                     'author': autor,
                     'publisher': editora,
@@ -31,6 +33,37 @@ def buscar_livro(request):
                 books.append(book)
     else:
         books = []
-        search_query = ''
+        bookId = ''
 
-    return render(request, 'buscar_livro.html', {'books': books, 'search_query': search_query})
+    return render(request, 'buscar_livro.html', {'books': books, 'bookId': bookId})
+
+def livro_detalhado(request, bookId):
+    if bookId:
+        url = f'https://www.googleapis.com/books/v1/volumes/{bookId}'
+        response = requests.get(url)
+        data = response.json()
+
+        livro = None
+        if 'volumeInfo' in data:
+            book_Id = data['id']
+            volume_info = data['volumeInfo']
+            titulo = volume_info.get('title', 'Desconhecido')
+            autor = ', '.join(volume_info.get('authors', ['Desconhecido']))
+            editora = volume_info.get('publisher', 'Editora desconhecida.')
+            ano_public = volume_info.get('publishedDate', '')
+            descricao = volume_info.get('description', 'Não Possui descrição.')
+            cover_url = volume_info['imageLinks']['thumbnail']
+            previewLink = volume_info.get('previewLink', 'Desconhecido')
+
+            livro = {
+                'id': book_Id,
+                'title': titulo,
+                'author': autor,
+                'publisher': editora,
+                'publishedDate': ano_public,
+                'descricao': descricao,
+                'cover_url': cover_url,
+                'previewLink': previewLink
+            }
+
+    return render(request, 'livro_detalhado.html', {'livro': livro})
